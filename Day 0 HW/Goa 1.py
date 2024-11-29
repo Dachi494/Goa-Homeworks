1a8edb74d761b062f89a4c24209310137707e18e
Dachi494 <dzamanasdachi58@gmail.com>
2024-11-29 12:48:48 +0400

    first commit

---
+++ Day 0 HW/Goa 1.py
@@ -0,0 +1,6 @@
+name = "dachi"
+surname = "dzamanashvili"
+age = 16
+height = 177.5
+goa= "chads"
+print("i am " + name + " " + surname + ", i am " + str(age) + " years old and " + str(height) + " in height also i think goa is for " + goa)
